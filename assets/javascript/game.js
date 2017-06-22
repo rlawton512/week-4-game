0dab7b82abd318701e293fa8aa554d577da371b0
rlawton512 <rocsi54321@gmail.com>
2017-06-22 16:00:48 -0400

    WIPv.3

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,92 @@
+	var userScore = 0;
+	var wins=0;
+	var losses=0;
+	
 
-var crstalNumberOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-var userScore =[];
-var wins=0;
-var losses=0;
-var randomNumberOptions = [];
+// function initializeGameStart(){
 
-$(document).ready(function(){
+// 	valueCrystal1 = "";
+// 	valueCrystal1 = "";
+// 	valueCrystal3 = "";
+// 	valueCrystal4 = "";
 
+// 	$("#crystal1, #crystal2, #crystal3, #crystal4").empty();
+// }
+
+var randomNumberBetween19and120 = Math.floor(Math.random()*121)+19;
+$("#random-number").html(randomNumberBetween19and120);
+
+
+//assign crystal 1 value 
+var crystalNumberOptions1 = Math.floor(Math.random()*13)+1;
+
+function crystalImageValues1 (){
+
+	$("#crystal1").attr("data-crystalvalue",crystalNumberOptions1);	
+	
+};
+
+crystalImageValues1();
+
+$("#crystal1").on("click", function(){
+	var crystalValue = ($(this).attr("data-crystalvalue"));
+	crystalValue = parseInt(crystalValue);
+
+	userScore += crystalValue;
+	console.log(userScore);
 });
 
+//assign crystal 2 value
+var crystalNumberOptions2 = Math.floor(Math.random()*13)+1;
+function crystalImageValues2 (){
 
-$(".crystal-images").on("click", function(){
-	alert("clicked");
+	$("#crystal2").attr("data-crystalvalue",crystalNumberOptions2);	
+	
+};
 
+crystalImageValues2();
+
+$("#crystal2").on("click", function(){
+	var crystalValue = ($(this).attr("data-crystalvalue"));
+	crystalValue = parseInt(crystalValue);
+
+	userScore += crystalValue;
+	console.log(userScore);
 });
 
-function generateNumbers (start,end){
+//assign crystal 3 value
+var crystalNumberOptions3 = Math.floor(Math.random()*13)+1;
+function crystalImageValues3 (){
 
-	for (; start<=end; start++)
-	{
-		randomNumberOptions.push(start);
-	}	
+	$("#crystal3").attr("data-crystalvalue",crystalNumberOptions3);	
+	
+};
 
-}
+crystalImageValues3();
 
-generateNumbers(19,120);
+$("#crystal3").on("click", function(){
+	var crystalValue = ($(this).attr("data-crystalvalue"));
+	crystalValue = parseInt(crystalValue);
 
-console.log(randomNumberOptions);
+	userScore += crystalValue;
+	console.log(userScore);
+});
 
+//assign crystal 4 value 
+var crystalNumberOptions4 = Math.floor(Math.random()*13)+1;
+function crystalImageValues4 (){
+
+	$("#crystal4").attr("data-crystalvalue",crystalNumberOptions4);	
+	
+};
+
+crystalImageValues4();
+
+$("#crystal4").on("click", function(){
+	var crystalValue = ($(this).attr("data-crystalvalue"));
+	crystalValue = parseInt(crystalValue);
+
+	userScore += crystalValue;
+	console.log(userScore);
+});
 
